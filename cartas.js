a865fc4605ebbb9d8300958589a55521f050235c
Amira García <amiragarcia@live.com>
2022-03-01 14:46:26 -0300

    Lo que tengo hasta ahora

---
+++ cartas.js
@@ -0,0 +1,62 @@
+let cartas =[
+    {
+        nombre: "Moria, the original",
+        img: "img/moria_original.jpg",
+        descripcion: "El meme en formato original",
+        valor: 1,
+    },
+    {
+        nombre: "Moria, rosa",
+        img: "img/moria_rosa.jpg",
+        descripcion: "Moria rosa", 
+        valor: 2,
+    },
+    {
+        nombre: "Moria, celeste",
+        img: "img/moria_celeste.jpg",
+        descripcion: "Moria celeste", 
+        valor: 1,
+    },
+    {
+        nombre: "Moria, amarilla",
+        img: "img/moria_amarilla.jpg",
+        descripcion: "Moria amarilla", 
+        valor: 2,
+    },
+    {
+        nombre: "Moria, verde",
+        img: "img/moria_verde.jpg",
+        descripcion: "Moria verde", 
+        valor: 1,
+    },
+    {
+        nombre: "Moria, lila",
+        img: "img/moria_lila.jpg",
+        descripcion: "Moria lila", 
+        valor: 2,
+    },
+    {
+        nombre: "Moria, en blanco y negro",
+        img: "img/moria_blanco&negro.jpg",
+        descripcion: "Moria en blanco y negro", 
+        valor: 1,
+    },
+    {
+        nombre: "Moria, sobresaturada",
+        img: "img/moria_sobresaturada.jpg",
+        descripcion: "Moria sobresaturada", 
+        valor: 2,
+    },
+    {
+        nombre: "Moria, el arte",
+        img: "img/moria_arte.jpg",
+        descripcion: "Cuadro artístico de Moria", 
+        valor: 1,
+    },
+    {
+        nombre: "Moria, en negativo",
+        img: "img/moria_negativo.jpg",
+        descripcion: "Moria en negativo",
+        valor: 2,
+    }
+]
